b952ac02af445d16f1fc61f14f2db5a9bdc64376
Polina1441 <14042003dp@mail.ru>
2024-05-16 22:51:01 +0500

    news screen

--- presentation/src/main/java/ru/skittens/ufagosport/ui/screens/main/news/NewsScreen.kt
+++ presentation/src/main/java/ru/skittens/ufagosport/ui/screens/main/news/NewsScreen.kt
@@ -1,6 +1,7 @@
 package ru.skittens.ufagosport.ui.screens.main.news
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,8 +18,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
@@ -27,6 +30,8 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FlashOn
 import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PersonAdd
 import androidx.compose.material.icons.filled.SportsBasketball
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.TripOrigin
@@ -48,6 +53,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -57,6 +63,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LifecycleStartEffect
@@ -76,9 +83,39 @@ import ru.skittens.ufagosport.ui.elements.TitleLargeText
 import ru.skittens.ufagosport.ui.elements.TitleMediumText
 import ru.skittens.ufagosport.ui.elements.TitleSmallText
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsScreen(newsViewModel: NewsViewModel = koinInject()) {
-
-}
-
+    val scrollState = rememberScrollState()
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .fillMaxHeight()
+            .padding(vertical = 6.dp, horizontal = 20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painterResource(R.drawable.empty_news), null, Modifier.fillMaxSize(1.0f))
+        Spacer(Modifier.height(18.dp))
+        TitleLargeText(
+            "Чтобы получать новости, добавляй друзей и начинайте вместе тренироваться",
+            Modifier.fillMaxWidth(.8f),
+            TextAlign.Center,
+            Color.White
+        )
+        Spacer(Modifier.height(18.dp))
+        FilledTonalButton(
+            {},
+            Modifier.fillMaxWidth(.9f),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.filledTonalButtonColors(Color(0xFF74FF79))
+        ) {
+            Row(Modifier.padding(6.dp), verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Default.PersonAdd, null, tint = Color.Black)
+                Spacer(Modifier.width(12.dp))
+                BodyLargeText("Добавить друга", color = Color.Black)
+            }
+        }
+        Spacer(Modifier.height(20.dp))
+    }
+    }
